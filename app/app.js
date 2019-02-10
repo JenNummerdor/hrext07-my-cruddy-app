61ab6827c16ce5a222908cabbad777eb3a803e89
Jen Nummerdor <jennifer.nummerdor@gmail.com>
2019-02-09 21:16:16 -0700

    SUPER EXCITING! Can read all data in localstorage.

--- app/app.js
+++ app/app.js
@@ -5,46 +5,73 @@ interact with localstorage
 
  */
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-  $(".btn-primary").on('click', function(e){
+  $(".btn-primary").on('click', function (e) {
+
     var selValue = $('input[name=exampleRadios]:checked').val();
-    console.log(selValue)
+
+    console.log("this is the local storage get item", localStorage.getItem('Sat Feb 09 2019 20:49:26 GMT-0700 (Mountain Standard Time)'))
     // write to db
-    localStorage.setItem(new Date(), selValue);
+    var date = new Date()
+    console.log(date)
+    localStorage.setItem(date, selValue);
   });
 
+  //will return all the cups in local storage
+  var getCupsInLocalStorage = (function () {
+    var cupsArr = []
+    for (var key in localStorage) {
+      if (typeof localStorage[key] === 'string') {
+        cupsArr.push(localStorage[key])
+      }
+    }
+    console.log(cupsArr)
+  })()
+
+  //will return all the dates in local storage
+  var getDatesInLocalStorage = (function () {
+    var datesArr = []
+    for (var key in localStorage) {
+      if (typeof key === 'string' && key.length > 10) {
+        datesArr.push(key)
+      }
+    }
+    console.log(datesArr)
+  })()
+
+  //update function will overwrite last entry written
 
   // delete item
-  $('.container-data').on('click', '.display-data-item', function(e){
+  $('.container-data').on('click', '.display-data-item', function (e) {
     console.log(e.currentTarget.dataset.keyvalue);
     var keyData = e.currentTarget.dataset.keyvalue;
     localStorage.removeItem(keyData);
     $('.container-data').text('');
   });
   // delete all?
-  $(".btn-danger").click(function(){
+  $(".btn-danger").click(function () {
     localStorage.clear();
     $('.container-data').text('');
   });
 
 });
 
-//below is what we did in class, I'm saving it since who knows what kinds of mischief I'll get into
-//var keyData = 'ourKey'; // going to need to make this dynamic?
-  // $(".btn btn-primary").on('click', function(e){
-  //   console.log(e);
-  //   var keyData = $('.input-key').val();
-  //   var valueData = $('.input-value').val();
-  //   // write to db
-  //   localStorage.setItem(keyData, valueData);
-  //   // read from db -> not sure I want to do this yet...
-  //   var displayText = keyData + ' | ' + localStorage.getItem(keyData);
-  //   // this only displays the last one? might want to switch to html
-  //   // and append a div
-  //   // <div class="display-data-item" data-keyValue="keyData">valueData</div>
-  //   // TODO make this vars make sense across the app
-  //   $('.container-data').html('<div class="display-data-item" data-keyValue="'+ keyData +'">'+valueData+'</div>');
-  //   $('.input-key').val('');
-  //   $('.input-value').val('');
-  // });
+// below is what we did in class, I'm saving it since who knows what kinds of mischief I'll get into
+// var keyData = 'ourKey'; // going to need to make this dynamic?
+//   $(".btn btn-primary").on('click', function(e){
+//     console.log(e);
+//     var keyData = $('.input-key').val();
+//     var valueData = $('.input-value').val();
+//     // write to db
+//     localStorage.setItem(keyData, valueData);
+//    
+// var displayText = keyData + ' | ' + localStorage.getItem(keyData);
+//     // this only displays the last one? might want to switch to html
+//     // and append a div
+//     // <div class="display-data-item" data-keyValue="keyData">valueData</div>
+//     // TODO make this vars make sense across the app
+//     $('.container-data').html('<div class="display-data-item" data-keyValue="'+ keyData +'">'+valueData+'</div>');
+//     $('.input-key').val('');
+//     $('.input-value').val('');
+//   });
--- app/chart.js
+++ app/chart.js
@@ -1,33 +1,53 @@
-  //the Graph
-  var chart = c3.generate({
-    bindto: '#chart',
-    data: {
-      columns: data,
-      type : 'pie',
-      labels: true,
-      colors: {
-          sleep: '#494344',
-          eat: '#625159',
-          code: '#7c4540',
-          relax: '#99424f',
-          other: '#9f5561',
-        }
-    },
-      pie: {
-      label: {
-          format: function (value) {
-              return value + ' hours';
-          },
-      },
-  },
-});
-
-var data = [
-  ['sleep', 8],
-  ['eat', 3],
-  ['code', 7],
-  ['relax', 2],
-  ['other', 4]
+var cups = [
+  ['Cups per Day', 1, 2, 3]
 ];
 
+var chart = c3.generate({
+  data: {
+      x: 'x',
+//        xFormat: '%Y%m%d', // 'xFormat' can be used as custom format of 'x'
+      columns: [
+          ['x', '2019-01-01', '2019-01-02', '2019-01-03', '2019-01-04', '2019-01-05', '2019-01-06'],
+          ['data1', 30, 200, 100, 400, 150, 250],
+      ]
+  },
+  axis: {
+      x: {
+          type: 'timeseries',
+          tick: {
+              format: '%Y-%m-%d'
+          }
+      }
+  }
+});
+
+
+
   //End of the Graph 
+
+  //   //the Graph
+  //   var chart = c3.generate({
+  //     bindto: '#chart',
+  //     data: {
+  //       columns: data,
+  //       type : 'pie',
+  //       labels: true,
+  //       colors: {
+  //           sleep: '#494344',
+  //           eat: '#625159',
+  //           code: '#7c4540',
+  //           relax: '#99424f',
+  //           other: '#9f5561',
+  //         }
+  //     },
+  //       pie: {
+  //       label: {
+  //           format: function (value) {
+  //               return value + ' hours';
+  //           },
+  //       },
+  //   },
+  // });
+  
+
+  
